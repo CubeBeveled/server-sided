ccd69977283af526ddd388789930bd3fe735321d
CubeBeveled <cubebeveled@gmail.com>
2024-07-15 15:48:42 +0200

    Added shebang to sh files

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-git --version
-java --version
+#!/bin/bash
 curl -OJ https://bevels-files.vercel.app/eula.txt
 curl -OJ https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.11/1.0.1/server/jar
 echo Done
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 java -Xms200M -XX:MaxRAMPercentage=95.0 -jar fabric-server-mc.1.20.1-loader.0.15.11-launcher.1.0.1.jar nogui
